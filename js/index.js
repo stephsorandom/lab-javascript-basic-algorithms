a5e754f1e3ee7efea9a68b9a9e6820b08685c8dd
Stephanie Mitchell <stephsorandom@gmail.com>
2021-06-07 14:22:44 -0400

    practicing JS fundamentals

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 // Iteration 1: Names and Input
-let hacker1=`Stephanie`
+let hacker1='Stephanie'
 console.log(`The driver's name is ${hacker1}`)
-let hacker2=`Mitchell`
+let hacker2='Mitchell'
 console.log(`The navigator's name is ${hacker2}`)
 
 
@@ -29,14 +29,14 @@ console.log(`The navigator's name is ${hacker2}`)
 //Print all characters in driverName
 //Separate characters by space and capitalize
 //driverName = Stephanie
-hacker1 = hacker1.toUpperCase()
-driverName = hacker1.split()
+hacker1 = hacker1.toUpperCase()//upper case
+driverName = hacker1.split('')//string to single character array string
 console.log(hacker1)
 
         //PART 2
 //Print navigatorName in reverse
-navigatorName = hacker2.split(``)
-console.log(navigatorName.reverse().join(``));
+navigatorName = hacker2.split('')
+console.log(navigatorName.reverse().join(''));
 
  //PART 3
 //Depending on lexicograhic order, print the following:
